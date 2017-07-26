af879be3064690038d1f5a4ceb39c151532b3717
Your Name <you@example.com>
2017-07-26 18:28:08 +0300

    Save Commit

--- dbhelper-utils/src/main/java/ru/rbt/dbhelper/utils/Main.java
+++ dbhelper-utils/src/main/java/ru/rbt/dbhelper/utils/Main.java
@@ -46,16 +46,78 @@ public class Main {
         } else {
             System.out.println("Please enter valid command!");
         }
-
+        ResultSet rs;
         String table;
-        try {
+        //                  SHOW_TABLE                                  SHOW_COLUMN                                           SHOW_CELLS                                                           SHOW_REV_TABLE                                                   SHOW_LINE_LIKE_THIS
+        if (finalComand == "SELECT * FROM BRM_ORDER" || finalComand == "SELECT CUSTOMER_ID FROM BRM_ORDER" || finalComand == "SELECT DATE_OF FROM BRM_ORDER WHERE ID_PK != '2'" || finalComand == "SELECT * FROM BRM_ORDER ORDER BY ID_PK DESC" || finalComand == "SELECT DATE_OF FROM BRM_ORDER WHERE DATE_OF LIKE '%2.07%'") {
+            try {
 
-            ResultSet rs = statement.executeQuery(finalComand);
+                rs = statement.executeQuery(finalComand);
 
-            //SHOW_TABLE
-            if (finalComand == "SELECT * FROM BRM_ORDER") {
+                //SHOW_TABLE or SHOW_REV_TABLE
+                if (finalComand == "SELECT * FROM BRM_ORDER" || finalComand == "SELECT * FROM BRM_ORDER ORDER BY ID_PK DESC") {
+                    table = String.format("\n\n\n\n%s\t%s\t\t%s", "ID_PK", "DATE_OF", "CUSTOMER_ID");
+                    System.out.println(table + "\n");
+                    while (rs.next()) {
+
+                        String idPk = rs.getString("ID_PK");
+                        String dateOf = rs.getString("DATE_OF");
+                        String customeId = rs.getString("CUSTOMER_ID");
+                        String output = String.format("%s\t%s\t\t%s", idPk, dateOf, customeId);
+                        System.out.println(output + "\n");
+                    }
+                }
+
+                //SHOW_COLUMN
+                if (finalComand == "SELECT CUSTOMER_ID FROM BRM_ORDER") {
+                    table = String.format("%s", "CUSTOMER_ID");
+                    System.out.println(table + "\n");
+                    while (rs.next()) {
+                        String customeId = rs.getString("CUSTOMER_ID");
+                        String output = String.format("%s", customeId);
+                        System.out.println(output + "\n");
+                    }
+                }
+
+                //SHOW_CELLS
+                if (finalComand == "SELECT DATE_OF FROM BRM_ORDER WHERE ID_PK != '2'") {
+                    table = String.format("\n\n%s", "DATE_OF");
+                    System.out.println(table + "\n");
+                    while (rs.next()) {
+                        String dateOf = rs.getString("DATE_OF");
+                        String output = String.format("%s", dateOf);
+                        System.out.println(output + "\n");
+                    }
+                }
+                //SHOW_LINE_LIKE_THIS
+                if (finalComand == "SELECT DATE_OF FROM BRM_ORDER WHERE DATE_OF LIKE '%2.07%'") {
+                    table = String.format("\n\n%s", "DATE_OF");
+                    System.out.println(table + "\n");
+                    while (rs.next()) {
+                        String dateOf = rs.getString("DATE_OF");
+                        String output = String.format("%s", dateOf);
+                        System.out.println(output + "\n");
+                    }
+                }
+
+                if (rs == null) {
+                    System.out.println("\n\nOhh, your statement is null!");
+                } else {
+                    System.out.println("\n\nStatement is not null.");
+                }
+            } catch (SQLException e) {
+                System.out.println("\n\n\n\n\n\n\n\t\t\t\t" + e.getErrorCode() + "\n\n\n\n\n\n\n");
+            }
+        }
+
+        if (finalComand == "UPDATE BRM_ORDER SET CUSTOMER_ID = '4',ID_PK = '2' WHERE DATE_OF = '12.07.2017'") {
+
+            try {
+
                 table = String.format("\n\n\n\n%s\t%s\t\t%s", "ID_PK", "DATE_OF", "CUSTOMER_ID");
+                System.out.println("\n\n\n\nBefore:\n\n");
                 System.out.println(table + "\n");
+                rs = statement.executeQuery("SELECT * FROM BRM_ORDER");
                 while (rs.next()) {
 
                     String idPk = rs.getString("ID_PK");
@@ -64,27 +126,27 @@ public class Main {
                     String output = String.format("%s\t%s\t\t%s", idPk, dateOf, customeId);
                     System.out.println(output + "\n");
                 }
-            }
 
-            //SHOW_COLUMN.
-            if (finalComand == "SELECT CUSTOMER_ID FROM BRM_ORDER") {
-                table = String.format("%s", "CUSTOMER_ID");
+                //Кидаем команду изменения
+                statement.execute(finalComand);
+
+
+
+                System.out.println("\n\n\n\nAfter:\n\n");
                 System.out.println(table + "\n");
+                rs = statement.executeQuery("SELECT * FROM BRM_ORDER");
                 while (rs.next()) {
+
+                    String idPk = rs.getString("ID_PK");
+                    String dateOf = rs.getString("DATE_OF");
                     String customeId = rs.getString("CUSTOMER_ID");
-                    String output = String.format("%s", customeId);
+                    String output = String.format("%s\t%s\t\t%s", idPk, dateOf, customeId);
                     System.out.println(output + "\n");
                 }
+            } catch (SQLException e) {
+                System.out.println("\n\n\n\n\n\n\n\t\t\t\t" + e.getErrorCode() + "\n\n\n\n\n\n\n");
             }
-            if (rs == null) {
-                System.out.println("\n\nOhh, your statement is null!");
-            } else {
-                System.out.println("\n\nStatement is not null.");
-            }
-        } catch (SQLException e) {
-            System.out.println("\n\n\n\n\n\n\n\t\t\t\t" + e.getErrorCode() + "\n\n\n\n\n\n\n");
         }
-
 
         try {
             connection.close();
