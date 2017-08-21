36d387e11ebfce82bedaf079a52138c788571a67
Mikhail Kryukov <KryukovMV@rbtechnologies.ru>
2017-08-21 17:40:03 +0300

    web servlet ann rem

--- dbhelper-war/src/main/java/ru/rbt/dbhelper/war/MyServlet.java
+++ dbhelper-war/src/main/java/ru/rbt/dbhelper/war/MyServlet.java
@@ -2,6 +2,7 @@ package ru.rbt.dbhelper.war;
 import ru.rbt.dbhelper.utils.QueryManager;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
